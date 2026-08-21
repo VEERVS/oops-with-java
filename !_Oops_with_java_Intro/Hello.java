41a3b210fc2722f8b32d6d2d7f336a6a37203c1c
Veer Vardhan Singh <veer232007@gmail.com>
2026-08-21 12:11:57 +0530

    Basic Intro To Java Oops Syntax

---
+++ !_Oops_with_java_Intro/Hello.java
@@ -0,0 +1,14 @@
+//Write a java program to print hello world
+
+class Hello{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println("Console loading...");
+    }
+}
+
+    class Test{
+        public static void main(String args[]){
+            System.out.println("Test");
+        }
+    }

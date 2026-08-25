21882d6ca964c87f5e57b49a5861df530f576f68
Veer Vardhan Singh <veer232007@gmail.com>
2026-08-25 10:32:26 +0530

    Calculate area of circle and rectangle using java oops

--- !_Oops_with_java_Intro/Test.java
+++ !_Oops_with_java_Intro/Test.java
@@ -1,7 +1,7 @@
 //Write a java program to print hello world
 
 class Hello{
-    public static void main(String args[]){
+    public static void  main(String args[]){
         System.out.println("Hello World");
         System.out.println("Console loading...");
     }

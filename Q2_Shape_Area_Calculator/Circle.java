89d7e513a558729b7dfa8d8fdebcb73ec9b5f109
Veer Vardhan Singh <veer232007@gmail.com>
2026-09-11 11:57:11 +0530

    Completed question 2 Shape Area Calculator

---
+++ Q2_Shape_Area_Calculator/Circle.java
@@ -0,0 +1,12 @@
+class Circle extends Shape {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ Q2_Shape_Area_Calculator/Shape.java
@@ -0,0 +1,7 @@
+abstract class Shape {
+    abstract double area();
+
+    void displayArea(){
+        System.out.println("Area = " + area());
+    }
+}

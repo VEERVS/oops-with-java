df1f5c53af0e73ddf2bb323e98ba41c14f5e8473
Veer Vardhan Singh <veer232007@gmail.com>
2026-09-11 16:17:54 +0530

    Completed question 4 ATM Withdrawal

---
+++ Q4_ATM_Withdrawal/UseCase4.java
@@ -0,0 +1,38 @@
+public class UseCase4 {
+
+    public static void main(String[] args) {
+
+        double balance = 5000.0;
+        double withdrawalAmount = 7500.0;
+
+        try {
+            withdraw(balance, withdrawalAmount);
+        }
+        catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            System.out.println("Transaction attempt completed.");
+        }
+    }
+
+    static void withdraw(double balance, double amount)
+            throws InsufficientBalanceException {
+
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance");
+        }
+
+        System.out.println(
+            "Withdrawal successful. New balance = " + (balance - amount)
+        );
+    }
+}
+
+
+class InsufficientBalanceException extends Exception {
+
+    InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
